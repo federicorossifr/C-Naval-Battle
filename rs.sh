ff90b095609e571abe0a17cd0ba7ac4512ae5a5c
federico <federico@osiris.lan>
2016-12-04 21:26:14 +0100

    Deleted old building files

--- rs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-gcc -c -Wall Server/server.c
-gcc -c -Wall Server/user.c
-gcc -c -Wall Server/handlers.c
-gcc -c -Wall Utilities/utility.c
-gcc handlers.o server.o user.o utility.o -o build/server
-./build/server 8080
